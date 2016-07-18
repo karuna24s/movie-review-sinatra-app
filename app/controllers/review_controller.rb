1afbd3744e5b1641f685265d9ebcccda99868e40
karuna24s <karuna24s@gmail.com>
2016-07-18 18:25:54 -0400

    add delete option to '/reviews/show.erb' and updated delete '/reviews' route

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -94,9 +94,7 @@ class ReviewController < ApplicationController
     else
       flash[:message] = "Looks like you weren't logged in yet. Please log in below."
       redirect to '/login'
+    end
   end
-
-
-
 
 end
