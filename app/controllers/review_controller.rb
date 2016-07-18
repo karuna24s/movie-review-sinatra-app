9f52e534c2b6b1d59b931a18708d140031f8484b
karuna24s <karuna24s@gmail.com>
2016-07-18 18:08:58 -0400

    created a delete request in order to delete a review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -83,7 +83,18 @@ class ReviewController < ApplicationController
   end
 
   # Delete
-
+  delete '/reviews/:id/delete' do
+    if is_logged_in?
+      @review = Review.find_by_id(params[:id])
+      if @review.critic_id == session[:critic_id]
+        @review.delete
+        flash[:message] = "The review was deleted."
+        redirect to '/reviews'
+      end
+    else
+      flash[:message] = "Looks like you weren't logged in yet. Please log in below."
+      redirect to '/login'
+  end
 
 
 
